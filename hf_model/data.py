73d4b011444ecff58605f969115c00f3302b669d
jahidhasanlinix <engjahidhasan20@gmail.com>
2022-10-23 09:51:26 -0500

    Update data.py
    comments

--- hf_model/data.py
+++ hf_model/data.py
@@ -16,3 +16,12 @@ tokenized_datasets = tokenizer.map(tokenize_function, batched=True)
 small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
 small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
 
+
+# text = "Here is the sentence I want embeddings for."
+# marked_text = "[CLS] " + text + " [SEP]"
+
+# # Tokenize our sentence with the BERT tokenizer.
+# tokenized_text = tokenizer.tokenize(marked_text)
+
+# # Print out the tokens.
+# print (tokenized_text)
