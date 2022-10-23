9e6a86349dec4cf6b01af7ec7f0d9ad5e719c630
jahidhasanlinix <engjahidhasan20@gmail.com>
2022-10-23 10:36:42 -0500

    Update data.py
    dataloader added

--- hf_model/data.py
+++ hf_model/data.py
@@ -1,4 +1,12 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+ 
+import pandas as pd 
 from transformers import AutoModel, AutoTokenizer
+from torch.utils.data import Dataset, DataLoader 
 
 model = AutoModel.from_pretrained("prajjwal1/bert-tiny")
 tokenizer = AutoTokenizer.from_pretrained(model)
@@ -25,3 +33,66 @@ small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(10
 
 # # Print out the tokens.
 # print (tokenized_text)
+
+# Defining some key variables 
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 4
+VALID_BATCH_SIZE = 4
+EPOCHS = 1
+LEARNING_RATE = 1e-05 
+
+# Create a Data Loader Class
+class CNNDailyMailData(Dataset):
+    def __init__(self, dataframe, tokenizer, max_len):
+        self.len = len(dataframe)
+        self.data = dataframe
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        
+    def __getitem__(self, index):
+        sentence = str(self.data.iloc[index].sents)
+        sentence = " ".join(sentence.split())
+
+        document = str(self.data.iloc[index].docs)
+        document = " ".join(document.split())
+
+        inputs = self.tokenizer.batch_encode_plus(
+            [sentence, document], 
+            add_special_tokens=True,
+            max_length=self.max_len,
+            padding="max_length",
+            return_token_type_ids=True,
+            truncation=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+
+        return {
+            'sent_ids': torch.tensor(ids[0], dtype=torch.long),
+            'doc_ids': torch.tensor(ids[1], dtype=torch.long),
+            'sent_mask': torch.tensor(mask[0], dtype=torch.long),
+            'doc_mask': torch.tensor(mask[1], dtype=torch.long),
+            'targets': torch.tensor([self.data.iloc[index].y], dtype=torch.long)
+        } 
+    
+    def __len__(self):
+        return self.len
+
+
+
+training_set = CNNDailyMailData(small_train_dataset, tokenizer, MAX_LEN)
+testing_set = CNNDailyMailData(small_eval_dataset, tokenizer, MAX_LEN)
+
+train_params = {'batch_size': TRAIN_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+test_params = {'batch_size': VALID_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+training_loader = DataLoader(training_set, **train_params)
+testing_loader = DataLoader(testing_set, **test_params)
+
