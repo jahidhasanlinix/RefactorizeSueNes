c894456d973a24b8d1bf0d50ac3e684c3a6e4b77
jahidhasanlinix <engjahidhasan20@gmail.com>
2022-10-29 12:08:46 -0500

    Update tut_suenes.py

--- hf_model/tut_suenes.py
+++ hf_model/tut_suenes.py
@@ -16,13 +16,19 @@ sequence_0 = "The real estate mogul nominated President Obama as well as his son
 sequence_1 = "For a period, Venezuela and Colombia were bitterly at odds. In the past year that relationship has healed. Obstacles to the strength of that relationship remain, analysts say."
 sequence_2 = "In the past year that relationship has healed."
 
+# print special token ids, Added the Special Word [CLS] at the begining and 
+# Special word [SEP] at the end
+model_inputs0 = tokenizer(sequence_0)
+print(model_inputs0["input_ids0"])
+tokens= tokenizer.tokenize(sequence_0)
+ids = tokenizer.convert_tokens_to_ids(tokens)
+print(ids)
+print(tokenizer.decode(model_inputs0["input_ids"]))
+print(tokenizer.decode(ids))
+
+
 # The tokenizer will automatically add any model specific separators (i.e. <CLS> and <SEP>) and tokens to
 # the sequence, as well as compute the attention masks.
-paraphrase = tokenizer(sequence_0, sequence_2, return_tensors="pt")
-not_paraphrase = tokenizer(sequence_0, sequence_1, return_tensors="pt")
-
-paraphrase_classification_logits = model(**paraphrase).logits
-not_paraphrase_classification_logits = model(**not_paraphrase).logits
-
-# paraphrase_results = torch.softmax(paraphrase_classification_logits, dim=1).tolist()[0]
-# not_paraphrase_results = torch.softmax(not_paraphrase_classification_logits, dim=1).tolist()[0]
+token_para = tokenizer(sequence_0, sequence_1, sequence_2, padding=True, truncation=True, return_tensors="pt")
+output = model(**token_para)
+output_logits = model(**token_para).logits
